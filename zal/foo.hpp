cb880804f491a62501cb2ef85d74626a3bad6d1b
Fixer1 <figat.wcale.nie@gmail.com>
2020-12-10 21:30:43 +0000

    13

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -10,8 +10,8 @@ std::vector< char > foo(std::list< Human >& people)
     std::vector< char > ret;
     ret.reserve(people.size());
     std::for_each(people.rbegin(), people.rend(), [&](auto& it) { it.birthday(); });
-    std::transform(people.rbegin(), people.rend(), ret.rbegin(), [&](auto& it) { 
-         ret.push_back(it.isMonster() ? 'n' : 'y');
+    std::transform(people.rbegin(), people.rend(), ret.rbegin(), [&](const auto& it) {
+        return it.isMonster() ? 'n' : 'y';
     });
     return ret;
 }
