25d96964e8632d93e47d8e531b6177dc31ebdc55
Fixer1 <figat.wcale.nie@gmail.com>
2020-12-10 13:09:47 +0000

    1

--- lab6.cpp
+++ lab6.cpp
@@ -1,6 +1,27 @@
+#include "make_random_vector.hpp"
 #include <iostream>
-
 int main()
 {
     puts("Hello, World!");
+    auto r = make_random_vector(5, 0, 10);
+    for (auto b : r) {
+        std::cout << b << std::endl;
+    }
+    std::sort(r.begin(), r.end());
+    std::cout << "sortowanie" << '\n';
+    for (auto a : r) {
+        std::cout << a << '\n';
+    }
+    int num_items2 = std::count(r.begin(), r.end(), 7);
+    std::cout << "number: "
+              << "7"
+              << " count: " << num_items2 << '\n';
+    int num_items3 = std::count_if(r.begin(), r.end(), [](int i) { return i >= 7; });
+    std::cout << "number greater than 7: "
+
+              << " count: " << num_items3 << '\n';
+    std::sort(r.begin(), r.end(), [](int a, int b) { return a > b; });
+    for (auto a : r) {
+        std::cout << a << " ";
+    }
 }
--- zal/foo.hpp
+++ zal/foo.hpp
@@ -2,15 +2,16 @@
 
 #include "Human.hpp"
 
-#include <algorithm>
 #include <list>
 #include <vector>
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    std::vector< char > ret_v(people.size());
-
-    // Twoja implementacja tutaj
-
-    return ret_v;
+   std::vector< char > ret;
+    ret.reserve(people.size());
+    transform(people.begin(), people.end(), ret.begin(), [](auto it) {
+        it->birthday();
+        ret.push_back(it->isMonster() ? 'n' : 'y');
+    });
 }
+
